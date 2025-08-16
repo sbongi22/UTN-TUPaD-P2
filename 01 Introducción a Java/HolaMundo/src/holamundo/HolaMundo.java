600019b994163f2af5c78164c50cff961ccd4943
Santiago <sbongi22@gmail.com>
2025-08-16 19:02:20 -0300

    Carga de TP 1

---
+++ 01 Introducción a Java/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sbong
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+//       //EJERCICIO 2
+//        System.out.println("¡Hola, Java!");
+        
+
+//       //EJERCICIO 3
+//        String nombre;
+//        int edad;
+//        double altura;
+//        boolean estudiante;
+//        
+//        nombre = "Santiago";
+//        edad = 30;
+//        altura = 1.80;
+//        estudiante = true;
+//        
+//        System.out.println("Nombre: " + nombre);
+//        System.out.println("Edad: " + edad + " años");
+//        System.out.println("Altura: " + altura + " metros");
+//        System.out.println("¿Es estudiante? " + estudiante);
+
+
+//       //EJERCICIO 4
+//        Scanner input = new Scanner(System.in);
+//        
+//        String nombre;
+//        int edad;
+//        
+//        System.out.print("Ingresá tu nombre: ");
+//        nombre = input.nextLine();
+//        System.out.print("Ingresá tu edad: ");
+//        edad = Integer.parseInt(input.nextLine());
+//        
+//        System.out.println("Tu nombre es " + nombre);
+//        System.out.println("Tienes " + edad + " años");
+
+
+//       //EJERCICIO 5
+//        Scanner input = new Scanner(System.in);
+//        
+//        int num1, num2, suma, resta, mult, div;
+//        
+//        System.out.print("Ingrese un primer número entero: ");
+//        num1 = Integer.parseInt(input.nextLine());
+//        System.out.print("Ingrese un segundo número entero: ");
+//        num2 = Integer.parseInt(input.nextLine());
+//        
+//        suma = num1 + num2;
+//        resta = num1 - num2;
+//        mult = num1 * num2;
+//        div = num1 / num2;
+//        
+//        System.out.println("Suma = " + suma);
+//        System.out.println("Resta = " + resta);
+//        System.out.println("Multiplicación = " + mult);
+//        System.out.println("División = " + div);
+        
+
+//       //EJERCICIO 6
+//        System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDirección: \"Calle Falsa 123\"");
+
+
+//       //EJERCICIO 8
+//        Scanner input = new Scanner(System.in);
+//        
+//        int num1, num2;
+//        double div1;
+//        
+//        System.out.print("Ingrese un primer número entero: ");
+//        num1 = Integer.parseInt(input.nextLine());
+//        System.out.print("Ingrese un segundo número entero: ");
+//        num2 = Integer.parseInt(input.nextLine());
+//        
+//        div1 = num1 / num2;
+//        
+//        System.out.println("Resultado (división double): " + div1);
+
+
+//       //EJERCICIO 9
+
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Ingresa tu nombre: "); 
+       //String nombre = scanner.nextInt(); // ERROR 
+        String nombre = scanner.nextLine(); // CORRECCION
+        System.out.println("Hola, " + nombre); 
+} 
+
+    }
+    
